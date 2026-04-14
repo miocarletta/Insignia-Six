c780fe22db3db0b92eb94e1dedd3083c49ceaec5
Mio Carletta <mio.carletta@gmail.com>
2026-04-15 01:03:28 +0700

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,36 +1,14 @@
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
-import { getMessaging, getToken, onMessage } from 'firebase/messaging';
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDfbXYAfQtjM6htcEqR",
+  apiKey: "API_KEY_KAMU",
   authDomain: "insignia-six.firebaseapp.com",
   projectId: "insignia-six",
-  storageBucket: "insignia-six.firebasestorage.app",
+  storageBucket: "insignia-six.appspot.com",
   messagingSenderId: "854049425936",
-  appId: "1:854049425936:web:aba18be6396f9a0d20d767",
-  measurementId: "G-QGD1Z4C8EL"
+  appId: "APP_ID_KAMU"
 };
 
-export const VAPID_KEY = "BCymawSIDV-GGgBnZyjADKE77P-w51OKigOOqx_3ICNY4OqYqeC6E-8vv4yAQkmaVbHeLrU9rriLLEGSda1bDFo";
-
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
-export const messaging = getMessaging(app);
-
-export async function requestNotifPermission() {
-  try {
-    const permission = await Notification.requestPermission();
-    if (permission === 'granted') {
-      const token = await getToken(messaging, { vapidKey: VAPID_KEY });
-      return token;
-    }
-    return null;
-  } catch (e) {
-    return null;
-  }
-}
-
-export function onForegroundMessage(callback) {
-  return onMessage(messaging, callback);
-}

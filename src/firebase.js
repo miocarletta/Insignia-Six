74a588e175598bfa486595dbc3e0288c9d0c5f22
Mio Carletta <mio.carletta@gmail.com>
2026-04-15 01:52:38 +0700

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "PASTE_API_KEY_ASLI_DISINI", 
+  apiKey: "AIzaSyDfbXYAfQtjM6htcEqRBTBRRo3HGggW8j4", 
   authDomain: "insignia-six.firebaseapp.com",
   projectId: "insignia-six",
   storageBucket: "insignia-six.appspot.com",
   messagingSenderId: "854049425936",
-  appId: "PASTE_APP_ID_ASLI_DISINI"
+  appId: "1:854049425936:web:7d6fda9b81e0b2de64d895"
 };
 
 const app = initializeApp(firebaseConfig);
